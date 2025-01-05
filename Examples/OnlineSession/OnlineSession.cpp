89cf693484ec6c6674050afb5ca276b0a8573473
irelia <irelia@macbook.pro>
2025-01-05 01:10:47 -0500

    changed up example to fit more into my game design

--- Examples/OnlineSession/OnlineSession.cpp
+++ Examples/OnlineSession/OnlineSession.cpp
@@ -1,18 +1,18 @@
-#include "SDL2/SDL.h"
+
 
 #define GEKKONET_STATIC
 
-#include "gekkonet.h"
+#include <raylib.h>
 
 #include <chrono>
-#include <string>
-#include <vector>
 #include <iostream>
+#include <string>
 #include <thread>
 #include <unordered_map>
+#include <vector>
 
-SDL_Window* window = nullptr;
-SDL_Renderer* renderer = nullptr;
+#include "gekkonet.h"
+
 bool running = false;
 
 using micro = std::chrono::microseconds;
@@ -26,35 +26,14 @@ using gclock = std::chrono::steady_clock;
 const int MAX_ROLLBACK_WINDOW = 8;
 
 bool init_window(void) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        fprintf(stderr, "Error initializing SDL.\n");
-        return false;
-    }
-    window = SDL_CreateWindow(
-        "GekkoNet Example: Online Session",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        300,
-        300,
-        0
-    );
-    if (!window) {
-        fprintf(stderr, "Error creating SDL Window.\n");
-        return false;
-    }
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer) {
-        fprintf(stderr, "Error creating SDL Renderer.\n");
-        return false;
-    }
+    InitWindow(400, 400, "Online Session");
+    SetExitKey(0);
+    SetTraceLogLevel(LOG_NONE);
+    SetTargetFPS(240);
     return true;
 }
 
-void del_window(void) {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
+void del_window(void) { CloseWindow(); }
 
 struct GInput {
     union inp {
@@ -63,30 +42,22 @@ struct GInput {
             char down : 1;
             char left : 1;
             char right : 1;
-        }dir;
+        } dir;
         unsigned char value;
-    }input;
+    } input;
 };
 
 void process_events() {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        switch (event.type) {
-        case SDL_QUIT:
-            running = false;
-            break;
-        case SDL_KEYDOWN:
-            if (event.key.keysym.sym == SDLK_ESCAPE) {
-                running = false;
-                break;
-            }
-        }
+    if (IsKeyPressed(KEY_ESCAPE)) {
+        running = false;
     }
 }
-
+struct Player {
+    GInput inputs;
+    Vector2 position;
+};
 struct GState {
-    int px[2]{ 0, 0};
-    int py[2]{ 0, 0};
+    Player players[2];
     char desync = 0;
     int framenumber = 0;
 };
@@ -98,23 +69,35 @@ std::unordered_map<int, GState> gamestates;
 void update_state(GState& gs, GInput inputs[2], int num_players) {
     // advance frame number
     gs.framenumber++;
-    // update game
+
+    // apply input
     for (int player = 0; player < num_players; player++) {
-        if (inputs[player].input.dir.up) gs.py[player] -= 2;
-        if (inputs[player].input.dir.down) gs.py[player] += 2;
-        if (inputs[player].input.dir.left) gs.px[player] -= 2;
-        if (inputs[player].input.dir.right)gs.px[player] += 2;
+        gs.players[player].inputs = inputs[player];
+    }
+    // update the gamestate
+    //  update game
+    for (int player = 0; player < num_players; player++) {
+        if (gs.players[player].inputs.input.dir.up)
+            gs.players[player].position.y -= 2;
+        if (gs.players[player].inputs.input.dir.down)
+            gs.players[player].position.y += 2;
+        if (gs.players[player].inputs.input.dir.left)
+            gs.players[player].position.x -= 2;
+        if (gs.players[player].inputs.input.dir.right)
+            gs.players[player].position.x += 2;
     }
 }
 
 // https://en.wikipedia.org/wiki/Fletcher%27s_checksum
 uint32_t fletcher32(const uint16_t* data, size_t len) {
     uint32_t c0, c1;
-    len = (len + 1) & ~1;      /* Round up len to words */
+    len = (len + 1) & ~1; /* Round up len to words */
 
-    /* We similarly solve for n > 0 and n * (n+1) / 2 * (2^16-1) < (2^32-1) here. */
-    /* On modern computers, using a 64-bit c0/c1 could allow a group size of 23726746. */
-    for (c0 = c1 = 0; len > 0; ) {
+    /* We similarly solve for n > 0 and n * (n+1) / 2 * (2^16-1) < (2^32-1)
+     * here. */
+    /* On modern computers, using a 64-bit c0/c1 could allow a group size of
+     * 23726746. */
+    for (c0 = c1 = 0; len > 0;) {
         size_t blocklen = len;
         if (blocklen > 360 * 2) {
             blocklen = 360 * 2;
@@ -161,8 +144,7 @@ void SaveGameState(GState* gs) {
 void save_state(GState* gs, GekkoGameEvent* ev) {
     // pass framenumber to gekkonet
     *ev->data.save.state_len = sizeof(int);
-    *ev->data.save.checksum =
-        fletcher32((uint16_t*)&gs->framenumber, sizeof(int));
+    *ev->data.save.checksum = fletcher32((uint16_t*)gs, sizeof(GState));
     std::memcpy(ev->data.save.state, &gs->framenumber, sizeof(int));
     // handle saving ourselves
     SaveGameState(gs);
@@ -187,16 +169,10 @@ void load_state(GState* gs, GekkoGameEvent* ev) {
 }
 
 void render_state(GState& gs) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    for (int player = 0; player < 2; player++)
-    {
-        SDL_Rect ball_rect = {
-            gs.px[player],
-            gs.py[player],
-            40,
-            40
-        };
-        SDL_RenderFillRect(renderer, &ball_rect);
+    for (int player = 0; player < 2; player++) {
+        Rectangle ball_rect = {(float)gs.players[player].position.x,
+                               (float)gs.players[player].position.y, 40, 40};
+        DrawRectangleRec(ball_rect, WHITE);
     }
 }
 
@@ -205,30 +181,25 @@ GInput get_key_inputs() {
     // reset inputs
     input.input.value = 0;
     // fetch local inputs
-    auto keys = SDL_GetKeyboardState(NULL);
     // local player
-    input.input.dir.up = keys[SDL_SCANCODE_W];
-    input.input.dir.left = keys[SDL_SCANCODE_A];
-    input.input.dir.down = keys[SDL_SCANCODE_S];
-    input.input.dir.right = keys[SDL_SCANCODE_D];
+    input.input.dir.up = IsKeyDown(KEY_W);
+    input.input.dir.left = IsKeyDown(KEY_A);
+    input.input.dir.down = IsKeyDown(KEY_S);
+    input.input.dir.right = IsKeyDown(KEY_D);
     return input;
 }
 
 micro GetFrameTime(float frames_ahead) {
     if (frames_ahead >= 1.0f) {
         return std::chrono::duration_cast<micro>(slow_frame(1));
-    }
-    else if (frames_ahead <= -1.0f) {
+    } else if (frames_ahead <= -1.0f) {
         return std::chrono::duration_cast<micro>(fast_frame(1));
-    }
-    else {
+    } else {
         return std::chrono::duration_cast<micro>(normal_frame(1));
     }
-
 }
 
-int main(int argc, char* args[])
-{
+int main(int argc, char* args[]) {
     std::vector<std::string> all_args;
 
     if (argc > 1) {
@@ -244,7 +215,8 @@ int main(int argc, char* args[])
     const int localport = std::stoi(all_args[1]);
     std::string remote_address = std::move(all_args[2]);
 
-    printf("lplayer:%d, lport:%d, rem_addr:%s\n", localplayer, localport, remote_address.c_str());
+    printf("lplayer:%d, lport:%d, rem_addr:%s\n", localplayer, localport,
+           remote_address.c_str());
 
     running = init_window();
 
@@ -256,12 +228,13 @@ int main(int argc, char* args[])
     int num_players = 2;
 
     GekkoSession* sess = nullptr;
-    GekkoConfig conf {};
+    GekkoConfig conf{};
 
     conf.num_players = num_players;
     conf.input_size = sizeof(char);
 
-    // instead of passing state we are passing the frame number and handling saving/loading ourselves.
+    // instead of passing state we are passing the frame number and handling
+    // saving/loading ourselves.
     conf.state_size = sizeof(int);
 
     conf.max_spectators = 0;
@@ -273,22 +246,24 @@ int main(int argc, char* args[])
     gekko_start(sess, &conf);
     gekko_net_adapter_set(sess, gekko_default_adapter(localport));
 
-    // this is order dependant so we have to keep that in mind. 
+    // this is order dependant so we have to keep that in mind.
     if (localplayer == 1) {
         // add local player
-        localplayer = gekko_add_actor(sess, LocalPlayer, nullptr); 
+        localplayer = gekko_add_actor(sess, LocalPlayer, nullptr);
         // add remote player
-        auto remote = GekkoNetAddress{ (void*)remote_address.c_str(), (unsigned int)remote_address.size() };
+        auto remote = GekkoNetAddress{(void*)remote_address.c_str(),
+                                      (unsigned int)remote_address.size()};
         gekko_add_actor(sess, RemotePlayer, &remote);
     } else {
         // add remote player
-        auto remote = GekkoNetAddress{ (void*)remote_address.c_str(), (unsigned int)remote_address.size() };
+        auto remote = GekkoNetAddress{(void*)remote_address.c_str(),
+                                      (unsigned int)remote_address.size()};
         gekko_add_actor(sess, RemotePlayer, &remote);
         // add local player
         localplayer = gekko_add_actor(sess, LocalPlayer, nullptr);
     }
 
-    gekko_set_local_delay(sess, localplayer, 1);
+    gekko_set_local_delay(sess, localplayer, 0);
     // timing
 
     gtime_point start_time(gclock::now());
@@ -301,11 +276,13 @@ int main(int argc, char* args[])
 
         auto frame_time = GetFrameTime(gekko_frames_ahead(sess));
 
-        std::cout << "ft: " << frame_time.count() << std::endl;
+        double sec_time = (double)frame_time.count() / 1000000;
+
+        // std::cout << "ft: " << frame_time.count() << std::endl;
 
         process_events();
 
-        //add local inputs to the session
+        // add local inputs to the session
         auto input = get_key_inputs();
         gekko_add_local_input(sess, localplayer, &input);
 
@@ -318,7 +295,9 @@ int main(int argc, char* args[])
 
             if (event->type == DesyncDetected) {
                 auto desync = event->data.desynced;
-                printf("desync detected, f:%d, rh:%d, lc:%u, rc:%u\n", desync.frame, desync.remote_handle, desync.local_checksum, desync.remote_checksum);
+                printf("desync detected, f:%d, rh:%d, lc:%u, rc:%u\n",
+                       desync.frame, desync.remote_handle,
+                       desync.local_checksum, desync.remote_checksum);
             }
 
             if (event->type == PlayerDisconnected) {
@@ -333,38 +312,41 @@ int main(int argc, char* args[])
             auto ev = updates[i];
 
             switch (ev->type) {
-            case SaveEvent:
-                printf("Save frame:%d\n", ev->data.save.frame);
-                save_state(&state, ev);
-                break;
-            case LoadEvent:
-                printf("Load frame:%d\n", ev->data.load.frame);
-                load_state(&state, ev);
-                break;
-            case AdvanceEvent:
-                // on advance event, advance the gamestate using the given inputs
-                inputs[0].input.value = ev->data.adv.inputs[0];
-                inputs[1].input.value = ev->data.adv.inputs[1];
-                current = ev->data.adv.frame;
-                printf("F:%d, P1:%d P2:%d\n", current, inputs[0].input.value, inputs[1].input.value);
-                // now we can use them to update state.
-                update_state(state, inputs, num_players);
-                break;
-            default:
-                printf("Unknown Event: %d\n", ev->type);
-                break;
+                case SaveEvent:
+                    // printf("Save frame:%d\n", ev->data.save.frame);
+                    save_state(&state, ev);
+                    break;
+                case LoadEvent:
+                    // printf("Load frame:%d\n", ev->data.load.frame);
+                    load_state(&state, ev);
+                    break;
+                case AdvanceEvent:
+                    // on advance event, advance the gamestate using the given
+                    // inputs
+                    inputs[0].input.value = ev->data.adv.inputs[0];
+                    inputs[1].input.value = ev->data.adv.inputs[1];
+                    current = ev->data.adv.frame;
+                    // printf("F:%d, P1:%d P2:%d\n", current,
+                    //      inputs[0].input.value, inputs[1].input.value);
+                    // now we can use them to update state.
+                    update_state(state, inputs, num_players);
+                    break;
+                default:
+                    printf("Unknown Event: %d\n", ev->type);
+                    break;
             }
         }
 
         // draw the state every iteration
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
+        BeginDrawing();
+        ClearBackground(BLACK);
         render_state(state);
-        SDL_RenderPresent(renderer);
+        EndDrawing();
 
         end_time = gclock::now();
 
-        auto work_time = std::chrono::duration_cast<micro>(end_time - start_time);
+        auto work_time =
+            std::chrono::duration_cast<micro>(end_time - start_time);
         // std::cout << "wt: " << work_time.count() << std::endl;
 
         auto adjusted_sleep = frame_time - work_time;
